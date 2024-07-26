def53848ad5424af033b5dec6e7ef704808c01cc
pcb97 <pcb1997@gmail.com>
2024-07-26 19:32:17 -0300

    Update rodar.py

--- rodar.py
+++ rodar.py
@@ -1,3 +1,4 @@
 import sys;sys.path.append(".")
 from dados.binance import BinanceClient
-binance = BinanceClient("","",False, True, idTelegram='')
+binance = BinanceClient("","",False, True, ChaveTelegram = '' ,idTelegram='')
+# binance.telegram.getID()
